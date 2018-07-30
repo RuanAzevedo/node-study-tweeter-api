6f957feeea947c9c3b97be851b5ef0814a0d3c92
Ruan Azevedo <ruan.azevedo@gmail.com>
2018-07-30 20:34:49 -0300

    first commit

---
+++ app/models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  followers: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
+  following: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+});
+
+UserSchema.pre('save', async function hashPassword(next) {
+  if (!this.isModified('password')) next();
+
+  this.password = await bcrypt.hash(this.password, 8);
+});
+
+UserSchema.methods = {
+  compareHash(password) {
+    return bcrypt.compare(password, this.password);
+  },
+};
+
+mongoose.model('User', UserSchema);
---
+++ app/routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const requireDir = require('require-dir');
+
+const routes = express.Router();
+
+const controllers = requireDir('./controllers');
+
+console.log(controllers);
+
+/**
+ * Auth
+ */
+routes.post('/signup', controllers.authController.signup);
+routes.post('/signin', controllers.authController.signin);
+
+module.exports = routes;
